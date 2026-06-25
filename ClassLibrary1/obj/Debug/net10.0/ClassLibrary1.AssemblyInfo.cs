2e3d7401fbd4f9b7f78dec45a1f363ddcb608977
Sierra Marlin <85563753+smarlin2021@users.noreply.github.com>
2026-06-24 21:20:52 -0400

    removed default pages

--- ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+084a14517dcef26e9cad334d390bf86d11563f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
